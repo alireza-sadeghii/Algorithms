2a0f8feb10cfa617c201039a9bf320f9405d5ee5
Alireza Sadeghi <alireza_sad@yahoo.com>
2023-03-22 14:05:22 +0430

    Bubble Sort Implementation

---
+++ src/main/java/ir/ac/kntu/sorting/Bubble.java
@@ -0,0 +1,27 @@
+/**
+ * @Author: Alireza Sadeghi
+ * Bubble Sort
+ */
+
+package ir.ac.kntu.sorting;
+
+import java.util.ArrayList;
+
+/**
+ * Bubble sort repeatedly swaps adjacent elements
+ * if they are in the wrong order until the list is sorted.
+ */
+public class Bubble {
+    public static void sort(ArrayList<Integer> input){
+        int tmp = 0;
+        for (int j = 0; j < input.size(); j++) {
+            for (int i = 0; i < input.size() - 1; i++) {
+                if (input.get(i) > input.get(i + 1)) {
+                    tmp = input.get(i + 1);
+                    input.set(i + 1, input.get(i));
+                    input.set(i, tmp);
+                }
+            }
+        }
+    }
+}
