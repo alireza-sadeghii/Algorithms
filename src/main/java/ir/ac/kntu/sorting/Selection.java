f73eeeebb57065c45fa9e8b03980d7d11c397b30
Alireza Sadeghi <alireza_sad@yahoo.com>
2023-03-22 13:12:41 +0430

    Selection Sort Implementation

---
+++ src/main/java/ir/ac/kntu/sorting/Selection.java
@@ -0,0 +1,30 @@
+/**
+ * @Author: Alireza Sadeghi
+ * Selection Sort
+ */
+
+package ir.ac.kntu.sorting;
+
+import java.util.ArrayList;
+
+public class Selection {
+    public static void sort(ArrayList<Integer> input){
+        int min_index = 0, min = 0, tmp = 0;
+
+        for (int i = 0; i < input.size() - 1; i++){
+            min_index = i;
+            min = input.get(i);
+
+            for (int j = i + 1; j < input.size(); j++){
+                if (input.get(j) < min){
+                    min_index = j;
+                    min = input.get(j);
+                }
+            }
+
+            tmp = input.get(min_index);
+            input.set(min_index, input.get(i));
+            input.set(i, tmp);
+        }
+    }
+}
